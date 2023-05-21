07f518897634835cbf9d236a24a484923a41bc10
Owen Medina <owensmedi@gmail.com>
2023-05-21 15:34:19 -0700

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Owen Medina on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
